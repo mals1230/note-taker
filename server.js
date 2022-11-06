62d3445628f8d3c9c3b4f4cb5572cc9ad26a3c51
Mallory S <110414321+mals1230@users.noreply.github.com>
2022-11-06 13:37:28 -0800

    outlined the rest of the routes I'll need

--- server.js
+++ server.js
@@ -17,9 +17,24 @@ app.get('/', (req, res) =>
 // GET Route for notes page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
-)
+);
+
+// GET Route to return all saved notes as JSON
+app.get('/api/notes', (req, res) =>
+
+);
+
+// POST Route to save new note, add to DB file, and return new note to client. 
+app.post('/api/notes', (req, res) =>
+
+);
+
+// DELETE Route to delete note using the noteID from page and DB
+app.delete('/api/notes/:id')
 
 
+
+// Connecting app to localhost
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
