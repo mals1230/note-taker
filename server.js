7dae8a9c014de09dcc7e86f80fb8227e8de41af5
Mallory S <110414321+mals1230@users.noreply.github.com>
2022-11-06 13:22:05 -0800

    brought in app.listen to connect to localhost3001

--- server.js
+++ server.js
@@ -9,8 +9,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// GET Route for homepage '/'
+// GET Route for homepage - '/'
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
