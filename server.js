8afc49c9535f3f4c04c1da3bd00d75be488f6d29
Mallory S <110414321+mals1230@users.noreply.github.com>
2022-11-06 13:27:46 -0800

    created note page GET route

--- server.js
+++ server.js
@@ -9,12 +9,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// GET Route for homepage - '/'
+// GET Route for homepage
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+)
 
 
 app.listen(PORT, () =>
