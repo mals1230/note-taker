801da4a714bcdfb303f42770a9a348c0fb52851a
Mallory S <110414321+mals1230@users.noreply.github.com>
2022-11-06 17:16:08 -0800

    fixed syntax errors on post route

--- server.js
+++ server.js
@@ -38,45 +38,34 @@ app.get('/api/notes', (req, res) =>
   })
 );
 
-
 // POST Route to save new note, add to DB file, and return new note to client 
-// app.post('/api/notes', (req, res) => {
-//   console.info(`${req.method} request received to add a new note`);
-//   // Destructuring assignment for the items in req.body
-//   const { title, text } = req.body;
+app.post('/api/notes', (req, res) => {
+    console.log('request recieved to add a new note');
+    const { title, text } = req.body
+    if (title && text) {
+        const NewNote = {
+            title,
+            text,
+            note_id: uuid(),
+        }
+    fs.readFile('./db.db.json', 'utf8', (err, note) => {
+        if (err) {
+            console.error(err);
+        } else {
+        const parsedNotes = JSON.parse(note);
+        parsedNote.push(newNote);
 
-//   // If all the required properties are present
-//   if (title && text) {
-//       // Variable for the object we will save
-//       const newNote = {
-//         title,
-//         text,
-//         note_id: uuid(),
-//       };
-
-//       fs.readFile('./db/db.json', 'utf8', (err, notes) => {
-//         if (err) {
-//           console.error(err);
-//         } else {
-//           // Convert string into JSON object
-//           const parsedNotes = JSON.parse(parsedNotes);
-  
-//           // Add a new note
-//           parsedNotes.push(newNote);
-
-
-  
-//           // Write updated note back to the file
-//           fs.writeFile(
-//             './db/db.json',
-//             JSON.stringify(parsedNotes, null, 4),
-//             (writeErr) =>
-//               writeErr
-//                 ? console.error(writeErr)
-//                 : console.info('Successfully updated note!')
-//         );
-//     };
-// });
+        fs.writeFile(
+            './db/notes.json',
+            JSON.stringify(parsedNotes, null, 4),
+            (writeErr) =>
+            writeErr 
+                ? console.error(writeErr)
+                : console.info ('note added successfully!')
+        );
+        };
+    });
+    }
 
 // BONUS - DELETE Route to delete note using the noteID from page and DB
 // app.delete('/api/notes/:id')
